c4fd28763d264f6c7f8ba0f69780f4da8227233c
Eric Winbigler <uzof99@gmail.com>
2019-06-27 13:03:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,16 @@ def monopoly_with_third_tier
          :three_pieces_owned=>100,
          :four_pieces_owned=>200
     },
-
+     
+     :names =>
+    {
+     :reading_railroad => {
+       "mortage_value"=>"$100"
+     },
+      :pennsylvania_railroad => {
+        "mortage_value"=>"$200"
+      },
+      
 end
 
 def monopoly_with_fourth_tier
